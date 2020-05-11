065bb5fca45ed5c8fd0449190178661ac410284a
Shubham Mude <shubhammude10201@gmail.com>
2020-05-11 14:30:52 +0530

    Created domain.py for parsing domain-name from url

---
+++ domain.py
@@ -0,0 +1,17 @@
+from urllib.parse import urlparse
+
+# Get the required domain name from url
+def domain_name(url):
+    try:
+        results = get_subdomain(url).split('.')
+        return results[-2] + '.' + results[-1]
+    except:
+        return ''
+
+
+# Parse the url
+def get_subdomain(url):
+    try:
+        return urlparse(url).netloc
+    except:
+        return ''
